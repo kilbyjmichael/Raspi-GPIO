448814abca1b173af60007ed12d4326716f28cee
Michael <kilbyjmichael@gmail.com>
2015-06-18 13:55:30 -0500

    Capacitive works with a wire... (also led)

--- capacitive_touch.py
+++ capacitive_touch.py
@@ -4,13 +4,16 @@ import time
 gpio.setmode(gpio.BCM)
 
 gpio.setup(21, gpio.IN)
+gpio.setup(4, gpio.OUT, initial=0)
 
 try:
     while True:
         input_state = gpio.input(21)
         if not input_state:
             print("Not Input Stat3 Detected")
-            print("\a")
+            gpio.output(4, 1)
+            time.sleep(.3)
+            gpio.output(4, 0)
 
 except KeyboardInterrupt:
     gpio.cleanup()
